f2cbee0dbe695c8baca5548ed3c3e7a64c9cbea5
Sumon <hemon.hasan123@gmail.com>
2025-10-01 12:57:25 +0600

    updated app.ts with index router, notFound and globalErrorHandlers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import { urls } from "./app/config/url.config";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import router from "./app/routes";
 
 const app: Application = express();
 
@@ -14,12 +17,15 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/api/v1", router);
+app.use("/api/v1", router);
 
 app.get("/", (_req: Request, res: Response) => {
   res.status(200).json({
     message: "SkillForge server is on: 😎",
   });
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
