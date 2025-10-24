9aff4646053538881a2c380b32f88d5efc69a9e9
Sumon <hemon.hasan123@gmail.com>
2025-10-24 20:18:54 +0600

    added user routes

---
+++ src/app/modules/user/user.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import checkAuth from "../../middlewares/checkAuth";
+import { UserControllers } from "./user.controller";
+
+const router = Router();
+
+router.patch("/me", checkAuth(), UserControllers.getMe);
+
+export const AuthRoutes = router;
